2c3c2716883d95f1c0b71393b85aaeae08136e6c
SamiMelhem <SaMiLMelhem23@gmail.com>
2024-04-22 20:18:51 +0000

    Checking for if tests pass on GitHub

--- environment/volumes/sniff.cpp
+++ environment/volumes/sniff.cpp
@@ -18,17 +18,32 @@ int main()
 	bpf_u_int32 net;
 
 	// Step 1: Open live PCAP session handle on NIC using your interface name
-	// TODO
+	// TODO:
+	handle = pcap_open_live("br-5738f31b4abe", BUFSIZ, 1, 1000, errbuf);
+	if (handle == nullptr) {
+		fprintf(stderr, "Couldn't open device %s: %s\n", "br-5738f31b4abe", errbuf);
+		return(2);
+	}
 
 	// Step 2: Compile filter_exp into BPF pseudo-code
-	// TODO
+	// TODO:
+	if (pcap_compile(handle, &fp, filter_exp, 0, net) == -1) {
+		fprintf(stderr, "Couldn't parse filter %s: %s\n", filter_exp, pcap_geterr(handle));
+		return(2);
+	}
+	if (pcap_setfilter(handle, &fp) == -1) {
+		fprintf(stderr, "Couldn't install filter %s: %s\n", filter_exp, pcap_geterr(handle));
+		return(2);
+	}
 
 	// Step 3: Capture packets
 	printf("Sniffing...\n");
-	// TODO
+	// TODO:
+	pcap_loop(handle, -1, got_packet, NULL);
 
 	// Close the PCAP session handle
-	// TODO
+	// TODO:
+	pcap_close(handle);
 
 	return 0;
 }
--- environment/volumes/spoof.cpp
+++ environment/volumes/spoof.cpp
@@ -14,13 +14,24 @@ int main()
     strncpy(data, CLIENT_IP, data_len);
 
     // create udp header
-    // TODO
+    // TODO:
+    udp->udp_dport = htons(9999);
+    udp->udp_sport = htons(12345);
+    udp->udp_ulen = htons(sizeof(struct udpheader) + data_len);
 
     // create ip header
-    // TODO
+    // TODO:
+    ip->iph_ver = 4;
+    ip->iph_ihl = 5;
+    ip->iph_ttl = 64;
+    ip->iph_sourceip.s_addr = inet_addr("192.168.0.1");
+    ip->iph_destip.s_addr = inet_addr("10.9.0.5");
+    ip->iph_protocol = IPPROTO_UDP;
+    ip->iph_len = htons(sizeof(struct ipheader) + sizeof(struct udpheader) + data_len);
 
     // send packet
-    // TODO
+    // TODO:
+    send_raw_ip_packet(ip);
 
     return 0;
 }
